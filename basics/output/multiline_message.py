c33704102ae0cb0b40eedf24745fe1c79accecc3
repl.it user <replituser@example.com>
2021-02-28 16:35:48 +0000

    Last updated to output

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standard output in multiple lines of code
+print("System Failure Imminent")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print ("hello")
+print("hello")
+print("first step: git add .")
+print("Second Step: git coommit -m \"your message here\"")
+print("Last step: git push")
