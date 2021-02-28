959d149b641b8ac20d16ae4bd4425474d354058d
repl.it user <replituser@example.com>
2021-02-28 16:50:24 +0000

    Escape Characters added

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#Displaying escape characters
+
+print("\n Displays a new line")
+print("\t Display a tab space")
+print("\\ Displays a backslah")
+print("\" Displays double quote")
+print("\' Displays single quote")
