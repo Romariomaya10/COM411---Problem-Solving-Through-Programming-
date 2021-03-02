693a85f34a0981313eb904d84747f2ccb0db7533
repl.it user <replituser@example.com>
2021-03-02 10:04:14 +0000

    week 1 demos completed

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("hello")
-print("first step: git add .")
-print("Second Step: git coommit -m \"your message here\"")
-print("Last step: git push")
+print("what is your name?")
+#Variable is a container, which can store data for us in the memory (string, integer, float , bool)
+name = input ()
+print ("what is your age?")
+age = int(input())
+print("what is your bank balance?")
+balance = float(input())
+
+print("welcome {}. You are said to be {} years old. Your bank balance is {}.".format(name, age, balance))
+  
