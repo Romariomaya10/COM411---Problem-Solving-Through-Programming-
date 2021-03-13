2a1f89d3d3586a939106a411286df12cf6376f2d
romariomaya10 <romario_maia@hotmail.com>
2021-03-13 23:11:52 +0000

    or operating

--- main.py
+++
@@ -1,9 +0,0 @@
-#We wish to create a program that allows Beep to pick an adventure.
-print("What type of adventure should I have?")
-adventure = input() 
-if ((adventure == "scary") or (adventure == "short")):
-  print("Entering the dark forest!")
-elif  ((adventure == "safe") or (adventure == "long")):
-  print ("Taking the safe route!")
-else:
-  print("Not sure which route to take.")
