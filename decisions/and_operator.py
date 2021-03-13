7c5fd46a0a15c4ce6dbfd6e55f8d35e15586f752
romariomaya10 <romario_maia@hotmail.com>
2021-03-13 23:26:16 +0000

    and operating

---
+++ decisions/and_operator.py
@@ -0,0 +1,10 @@
+#Beep is in the forest looking for a way out
+
+print("What did I hear?")
+hear = input()
+print("What did I see!")
+see = input()
+if ((hear == "grrr") and (see == "two red eyes")):
+  print("There is a scary creature. I should get out of here!")
+else:
+  print (" I am a little scared but I will contiune")
