462c8e26cd1c3c44643159b7d1d4146892a1f6b3
romariomaya10 <romario_maia@hotmail.com>
2021-03-13 23:40:39 +0000

    review creating story

---
+++ decisions/review.py
@@ -0,0 +1,13 @@
+#Beep is playing Weekend League on FIFA21 we want to know how is he doing on this game
+print ("How is your weekend league?")
+fifa = input() 
+if (fifa == "bad"):
+  print("Sorry to hear this!")
+else:
+   print("Keep it up!")
+print("Have you ever reached elite")
+elite = input()
+if ((elite == "elite 1") or (elite == "elite 2") or (elite == "elite 3")):
+  print("Well done! This is impressive!")
+else:
+  print("Wow Beep! You are a newbie! HAHAHA!")
