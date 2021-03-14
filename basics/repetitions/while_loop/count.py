9252f9be2e021da5dbc5cd7fb013e6aa7eff5b14
romariomaya10 <romario_maia@hotmail.com>
2021-03-14 14:37:57 +0000

    ccounting with while loop

--- basics/repetitions/while_loop/count.py
+++ basics/repetitions/while_loop/count.py
@@ -1,10 +1,10 @@
 #We wish to create a program that allows Beep to avoid live cables.
 
 print("How many live cables must I avoid?")
-cable = int(input())
-avoid = 1
-while (avoid <= cable ):
+cable = int(input()) #user to enter the number of cables that beep will avoid
+avoid = 1 #number 1 is to be added as it will be counting the cables 1 by 1
+while (avoid <= cable ): #avoid cables is < or = to the number of cables
   print ("Avoiding...")
-  print (f"...done! {avoid} live cable avoided!")
+  print (f"...done! {avoid} live cable avoided!") #f for format to make sure the {avoid} will be display counting untill the final number entered by the user
   avoid = avoid + 1
 print("All live cables have been avoided")
