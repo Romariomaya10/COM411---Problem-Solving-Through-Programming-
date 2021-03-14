ee2dc5e300ea3cac87f32d9d9f13abba60d8a3dd
romariomaya10 <romario_maia@hotmail.com>
2021-03-14 14:07:54 +0000

    while loop

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 #We wish to create a program that allows Beep to remove the cables holding the robot.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("How many cables should I remove?")
 cable = int(input())
 while (cable > 0):
  print("Removed cable")
  cable = cable - 1
 print ("completed")
-
-
-
-
-
-
 
