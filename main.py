79b080e0251c2bca7ecaa858f5a5130eecdfe549
repl.it user <replituser@example.com>
2021-03-16 10:26:13 +0000

    fiding number prime

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-#We wish to create a program to help the robot communicate with Beep.
-print("Please enter a phrase?")
-phrase = input()
-bops = 0
-while (bops < len(phrase)):
-    print("Bop ", end="")
-    bops = bops + 1
+def isPrime(x):
+  for i in range(2, x):
+    if x%i == 0:
+      return False
+  return True
+
+def findPrime(beginning, finish):
+  for j in range(beginning, finish):
+    if isPrime(j):
+      return j
+
+def encrypt():
+  print("Provide two intergers")
+  x = int(input())
+  y = int(input())
+  prime1 = findPrime(x,y)
+  print("Provide two more intergers")
+  x = int(input())
+  y = int(input())
+  prime2 = findPrime(x,y)
+  return prime1*prime2
+  
+print(encrypt())
